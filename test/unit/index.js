324fac87f11edea9f27a8f4f8fe57bb470b0c8c0
Julian Grinblat <julian@dotcore.co.il>
2026-07-14 04:43:31 +0900

    chore: remove redundant dirname-filename-esm

--- test/unit/index.js
+++ test/unit/index.js
@@ -1,13 +1,12 @@
 import { join } from 'node:path';
 import { readFileSync } from 'node:fs';
-import { dirname } from 'dirname-filename-esm';
 import { expect } from 'chai';
 
 // https://github.com/import-js/eslint-plugin-import/issues/1649
 // eslint-disable-next-line import/no-unresolved
 import { name, lua, numberOfKeys } from 'redis-pdel';
 
-const pdel = readFileSync(join(dirname(import.meta), '..', '..', 'src', 'pdel.lua'), 'utf8');
+const pdel = readFileSync(join(import.meta.dirname, '..', '..', 'src', 'pdel.lua'), 'utf8');
 
 describe('unit', () => {
   it('should export correct object literal from esm build', () => {
